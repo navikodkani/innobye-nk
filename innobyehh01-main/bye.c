30fce2afc2da6508e657e8d983be629436e342bd
navikodkani <82422230+navikodkani@users.noreply.github.com>
2023-12-13 12:35:13 -0800

    Add files from local

---
+++ innobyehh01-main/bye.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envp) {
+    printf("Bye bye innomedia 1\n");
+    return 0;
+}
